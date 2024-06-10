11f62e22ed19558eb7460fdc236bfc2a6749a6db
Bowi3 <leonnardofender@gmail.com>
2024-06-10 10:24:33 -0300

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -8,21 +7,48 @@ public class Main {
         Curso curso1 = new Curso();
         curso1.setTitulo("Curso Java");
         curso1.setDescricao("Descrição");
+        curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("Curso C#");
         curso2.setDescricao("Descrição 2");
-
-        System.out.println(curso1);
-        System.out.println(curso2);
+        curso2.setCargaHoraria(12);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria Java");
         mentoria.setDescricao("Descrição");
         mentoria.setData(LocalDate.now());
 
-        curso1.setCargaHoraria(8);
-        curso2.setCargaHoraria(12);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devLeo = new Dev();
+        devLeo.setNome("Léo");
+        devLeo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos: " + devLeo.getConteudosInscritos());
+        devLeo.progreir();
+        devLeo.progreir();
+        devLeo.progreir();
+        System.out.println("-");
+        System.out.println("Conteúdos: " + devLeo.getConteudosConcluidos());
+        System.out.println("Conteúdos: " + devLeo.getConteudosInscritos());
+        System.out.println("XP: " + devLeo.calcularTotalXp());
+
+        System.out.println("-------");
+
+        Dev devLevi = new Dev();
+        devLevi.setNome("Levi");
+        devLevi.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos: " + devLevi.getConteudosInscritos());
+        devLevi.progreir();
+        devLevi.progreir();
+        System.out.println("-");
+        System.out.println("Conteúdos: " + devLevi.getConteudosConcluidos());
+        System.out.println("Conteúdos: " + devLevi.getConteudosInscritos());
+        System.out.println("XP: " + devLevi.calcularTotalXp());
     }
 }
